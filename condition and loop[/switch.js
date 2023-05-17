856a31aa7a462300b526eabf7696b7eadbae6095
Narayan-Magar <124230893+Narayan-Magar@users.noreply.github.com>
2023-05-17 10:50:36 +0545

    darkmode

--- condition and loop[/switch.js
+++ condition and loop[/switch.js
@@ -1,26 +1,26 @@
-let myDAy =new Date().getDate();
+let myDAy = new Date().getDate();
 switch(myDAy){
     case 0:
-    console.log("sunday")
+    console.log("sunday");
     break;
     case 1:
-    console.log("monday")
+    console.log("monday");
     break:
     case 2:
-    console.log("tuesday")
+    console.log("tuesday");
     break;
     case 3:
-    console.log("wednesday")
+    console.log("wednesday");
     break;
     case 4:
-    console.log("thursday")
+    console.log("thursday");
     break;
     case 5:
-    console.log("friday")
+    console.log("friday");
     break;
     case 6:
-    console.log("saturday")
+    console.log("saturday");
     break;
     default:
-    console.log("error")
+    console.log("error");
 }
